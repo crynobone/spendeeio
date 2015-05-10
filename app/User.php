7106dbf2a14df33e39bec715e227261693bf51c1
crynobone <crynobone@gmail.com>
2015-05-11 00:42:12 +0800

    Update basic setup.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- app/User.php
+++ app/User.php
@@ -4,6 +4,9 @@ use Orchestra\Model\User as Eloquent;
 
 class User extends Eloquent
 {
+    /**
+     * Available subscription types.
+     */
     const SUBSCRIPTION_FOREVER = 'forever';
     const SUBSCRIPTION_TRIAL   = 'trial';
     const SUBSCRIPTION_YEARLY  = 'yearly';
